73ff2b090da4a243b263b7ac12625503cc95baf5
Lucasz-Niemiec <lucasniemieczenarruza21@gmail.com>
2022-03-02 12:44:36 -0300

    added client to cors whotelist

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,4 +1,8 @@
-const whitelist = ["http://127.0.0.1:5500"];
+const whitelist = [
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+  "https://confident-jennings-d10152.netlify.app/",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
